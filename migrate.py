48e54bbc99dee45ae4969e7cd13e4a38e9d2ff78
D Croft <96200199+ddcroft73@users.noreply.github.com>
2021-12-20 19:43:49 -0500

    Add files via upload
    refactor varaibles

--- migrate.py
+++ migrate.py
@@ -28,32 +28,32 @@ class FileMigration:
     # the files in the "sort Path" are used as "keys" in a dictionary that contains
     # the destinations of each file, "values". 
     def disperse_files(self) -> None:
-        _json_data  = ConfigureJson.get_data(self.json_fname, self.default_path)
-        self.spath =  list(_json_data.keys())[0]
-        self.key_list = self.__make_keylist(_json_data) 
+        json_data  = ConfigureJson.get_data(self.json_fname, self.default_path)
+        self.spath =  list(json_data.keys())[0]
+        self.key_list = self.__make_keylist(json_data) 
         # if no files match the keys in the json_data dict, nothing to do
         if not self.key_list: 
             status_report(self.status, "No relevant file types found.\nNothing to do") 
             self.log_move(note=" Files not found, attempted move at", do_time=True)             
         else:
-            _from_to_dict = self.__construct_dict(_json_data)
-            self.__exec_move_instructions(_from_to_dict)
+            from_to_dict = self.__construct_dict(json_data)
+            self.__exec_move_instructions(from_to_dict)
        
     # get all files to be moved
     def __make_keylist(self, json_data: dict) -> list:
-        _spath = self.spath
-        _types = list(json_data[_spath].keys())        
-        return [os.path.join(_spath,file) for file in os.listdir(_spath) if getfile_ext(file) in _types]
+        spath = self.spath
+        types = list(json_data[spath].keys())        
+        return [os.path.join(spath,file) for file in os.listdir(spath) if getfile_ext(file) in types]
     
     # make a dict to guide the migration key = From : Value = To
     def __construct_dict(self, json_data: dict) -> dict:
-        _paths = {}
-        _spath = self.spath
+        paths = {}
+        spath = self.spath
         for item in self.key_list:
-            for key, value in json_data[_spath].items(): 
+            for key, value in json_data[spath].items(): 
                 if getfile_ext(item) == key:
-                     _paths[item] = os.path.join(value, os.path.basename(item))
-        return _paths
+                     paths[item] = os.path.join(value, os.path.basename(item))
+        return paths
 
       # move em one by one        
     def __exec_move_instructions(self, paths) -> None:
@@ -74,19 +74,19 @@ class FileMigration:
     
     # Keeps track of the actions and stores a text file in the sort path
     def log_move(self, from_path: str=None, to_path=None, stamp: bool=False, note: str=None, do_time: bool=False) -> None:
-        _now = datetime.now()
-        _date = _now.strftime("%b-%d-%Y") 
-        _time = _now.strftime("%H:%M:%S") 
+        now = datetime.now()
+        date = now.strftime("%b-%d-%Y") 
+        time = now.strftime("%H:%M:%S") 
         fname = os.path.join(self.spath, "migrate_log.txt")
+        
         if from_path and to_path:
             output = f"\nMOVED:  {os.path.basename(from_path)} \nFROM:   {getdir_only(from_path)}  \nTO:     {getdir_only(to_path)}"
         if stamp:
-            output = f"\n----------------------------- MIGRATION COMPLETED at: ({_time}) on: ({_date}) ----------------------------------"    
+            output = f"\n----------------------------- MIGRATION COMPLETED at: ({time}) on: ({date}) ----------------------------------"    
         if note:
             output = note
         if do_time:
-            output = note + f" Time: {_time}  Date: {_date}"
-
+            output = note + f" Time: {time}  Date: {date}"
 
         with open(fname, "a") as f:
             f.write(output + "\n")
